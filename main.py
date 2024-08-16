a1b3fa314fadecb803bfb9441c78f0c489b98925
Sebastian Fridtun <sebastian.fridtun@gmail.com>
2024-08-16 20:38:13 +0200

    Print first 10 lines

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ from PIL import Image
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    img = Image.open('snail.bmp')
+    img = Image.open('wolf.bmp')
+    rgb_im = img.convert('RGB')
+    for row in range(10):
+        first_row = [rgb_im.getpixel((i, row)) for i in range(rgb_im.size[0])]
+        print(first_row)
+        print(first_row.count((0, 0, 0)))
+        print(len(first_row))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
